40ed6c3e14216d0febc0eadac44716b952439966
Chun <caochun@gmail.com>
2022-02-25 15:29:36 +0800

    init commit

---
+++ src/test/java/info/nemoworks/chartd/ChartdApplicationTests.java
@@ -0,0 +1,13 @@
+package info.nemoworks.chartd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChartdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
